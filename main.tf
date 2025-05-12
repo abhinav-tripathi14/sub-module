6f7442eb89418e47cbc879b1cf07e5fc94615f2e
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-05-12 14:58:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.21.0"
+    }
+  }
+}
+
+provider "aws" {
+ region="us-west-1"
+}
+
 module "vpc_vpc-endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   version = "5.21.0"
